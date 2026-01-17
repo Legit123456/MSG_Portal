136a3b02ffcdb7af674303ef7169ea0ce461eef1
Legit123456 <umartimi678@gmail.com>
2026-01-17 14:32:33 +0100

    Fix global variable for Vercel

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -6,5 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5176, // 👈 Explicitly set to 5176
-  }
+  },
+  define: {
+    global: 'window',
+  },
 })
--- client/vite.config.js
+++ client/vite.config.js
@@ -6,5 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5177, // 👈 Explicitly set to 5177
-  }
+  },
+  define: {
+    global: 'window',
+  },
 })
